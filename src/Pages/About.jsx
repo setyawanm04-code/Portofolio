fae0b89cff7c93e06d3973528c27ecb73a0afde4
setyawan <setyawanm04@gmail.com>
2026-09-04 16:20:59 +0300

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -265,7 +265,7 @@ Saya berfokus pada penciptaan pengalaman digital yang menarik dan selalu berupay
       </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1lfJxOXWzYE8u3OEwN4Ziy9-185Z8pVjO/view?usp=drivesdk" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/drive/folders/18a7gDAXrye3pxcv38VUqwLaEfyFH-e9R" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
